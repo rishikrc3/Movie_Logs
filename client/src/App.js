e9a1a6c8c6ecd871c7c57319b3fab27867993f32
Rishik Chakravarty <rishikrc3@gmail.com>
2024-02-20 22:21:28 +0530

    axios_etc

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,29 @@
 import React from "react";
-import "./index.css";
-function App() {
-  return <div className="text-3xl font-bold underline">Hello</div>;
-}
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+const App = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("http://localhost:3000/movies");
+        setMovies(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  console.log(movies);
+
+  return (
+    <>
+      <div>Hi</div>
+    </>
+  );
+};
 
 export default App;
