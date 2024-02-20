8d9da28a931b8c0fc3acbd0c4cbca8449e813f56
Rishik Chakravarty <rishikrc3@gmail.com>
2024-02-20 12:45:08 +0530

    fetching_from_db

--- server/index.js
+++ server/index.js
@@ -1,7 +1,25 @@
+const pg = require("pg");
 const express = require("express");
 
 const app = express();
 
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "Movie_Logs",
+  password: "12345",
+  port: 5432,
+});
+
+db.connect();
+app.get("/movies", async (req, res) => {
+  try {
+    const result = await db.query("SELECT * FROM movies");
+    res.json(result.rows);
+  } catch (error) {
+    console.error(error);
+  }
+});
 app.listen(3000, () => {
   console.log("Server is on port 3000");
 });
