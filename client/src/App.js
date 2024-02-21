fe1535ea427777f4234d48912d2018b1486e43b8
Rishik Chakravarty <rishikrc3@gmail.com>
2024-02-21 11:59:59 +0530

    omdbApi_call

--- client/src/App.js
+++ client/src/App.js
@@ -3,20 +3,21 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import Body from "./components/Body";
 import Header from "./components/Header";
+import "./index.css";
 const App = () => {
   const [movies, setMovies] = useState([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get("http://localhost:3000/movies");
-        setMovies(response.data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    fetchData();
-  }, []);
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const response = await axios.get("http://localhost:3000/movies");
+  //       setMovies(response.data);
+  //     } catch (error) {
+  //       console.error(error);
+  //     }
+  //   };
+  //   fetchData();
+  // }, []);
 
   console.log(movies);
 
--- client/src/components/Body.js
+++ client/src/components/Body.js
@@ -1,6 +1,26 @@
 import React from "react";
-
+import axios from "axios";
+import { useState, useEffect } from "react";
 const Body = () => {
+  const [listOfMovies, setListofMovies] = useState([]);
+  const [movieName, setMovieName] = useState("Attack on Titan");
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(
+        "https://www.omdbapi.com/?apikey=66263dea&s=" + movieName + "#"
+      );
+      setListofMovies(response.data.Search);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  console.log(listOfMovies);
   return <div>Hi this is the body</div>;
 };
 
